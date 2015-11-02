1d6ff64072bc26df1272243ff75ea61e91b3868c
Aiden Tepper <aidenjtep@gmail.com>
2015-11-01 22:27:47 -0800

    Added names/js framework

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ accessToken = "4fdb248ad771e101c196f31e5be93dffaa247d6994ebe490e303c1e55f1970ed"
 deviceID = "243c200bfb71";
 url = "https://api-http.littlebitscloud.cc/v2/devices/" + deviceID + "/output";
 
-rgbtest = function(){
-  http.post(url, {percent:100, duration_ms:4000});
-};
+
+
+function call1() = {
+
+}
+
+function call2() = {
+
+}
+
+function call3() = {
+
+}
+
+function call4() = {
+
+}

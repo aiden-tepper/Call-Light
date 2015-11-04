ca2f660f86eb7a73f5da1a967da8d5935fb1c9a7
Aiden Tepper <aidenjtep@gmail.com>
2015-11-03 20:41:44 -0800

    Created post() function

--- js/app.js
+++ js/app.js
@@ -14,9 +14,36 @@ accessToken4 = "";
 deviceID4 = "";
 url4 = "https://api-http.littlebitscloud.cc/v2/devices/" + deviceID4 + "/output";
 
-$("#button1").click(function(){
+function post() {
   console.log("clicked");
-  $.post("http://api-http.littlebitscloud.cc/v2/devices/243c200bfb71/output", {power: 100, duration_ms: 5000}, function(data, status){
-    alert("Data: " + data + "\nStatus: " + status);
+
+  $.ajax({
+
+    contentType: "application/json",
+    url: url1,
+    type: "post",
+
+    data: {
+      access_token: accessToken1,
+      power: 100,
+      duration_ms: 5000
+    },
+
+    headers: {
+      authorization: "Bearer 4fdb248ad771e101c196f31e5be93dffaa247d6994ebe490e303c1e55f1970ed",
+      accept: "application/vnd.littlebits.v2+json"
+    },
+
+    dataType: "json",
+
+    success: function(result, status, xhr){
+      console.log("result: " + result + "\nStatus: " + status + "\nXHR:" + JSON.stringify(xhr));
+    },
+
+    error: function(xhr, status, error){
+      console.log("XHR: " + JSON.stringify(xhr) + "\nStatus: " + status + "\nError: " + error);
+    }
+
   });
-});
+
+}

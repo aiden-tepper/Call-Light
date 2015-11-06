04322547d8a4888b133e24ec4794dfbd588a33c7
Aiden Tepper <aidenjtep@gmail.com>
2015-11-05 19:22:07 -0800

    Created test() function

--- js/app.js
+++ js/app.js
@@ -14,13 +14,44 @@ accessToken4 = "";
 deviceID4 = "";
 url4 = "https://api-http.littlebitscloud.cc/v2/devices/" + deviceID4 + "/output";
 
+
+function test() {
+  $.ajax({
+
+    url: "https://api-http.littlebitscloud.cc/v2/devices/243c200bfb71/output",
+    method: "POST",
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    beforeSend: function(jqXHR) {
+      jqXHR.setRequestHeader("Authorization", "Bearer 4fdb248ad771e101c196f31e5be93dffaa247d6994ebe490e303c1e55f1970ed");
+    },
+    headers: {
+      //authorization: "Bearer 4fdb248ad771e101c196f31e5be93dffaa247d6994ebe490e303c1e55f1970ed"
+    },
+    data: {
+      "power": 100,
+      "duration_ms": 5000
+    },
+
+    success: function(result, status, xhr){
+      console.log("result: " + result + "\nStatus: " + status + "\nXHR:" + JSON.stringify(xhr));
+    },
+    error: function(xhr){
+      console.log("An error occured: " + xhr.status + " " + xhr.statusText);
+    }
+
+  });
+}
+
+
 function post() {
 
   console.log("clicked");
 
   $.ajax({
 
-    url: url1,
+    //url: url1,
+    url: "https://api-http.littlebitscloud.cc/v2/devices/243c200bfb71/output",
     type: "post",
     dataType: "json",
 
@@ -32,7 +63,7 @@ function post() {
     headers: {
       "Accept": "application/vnd.littlebits.v2+json",
       "Content-Type": "application/json",
-      "Authorization": "Bearer 4fdb248ad771e101c196f31e5be93dffaa247d6994ebe490e303c1e55f1970ed"
+      //"Authorization": "Bearer 4fdb248ad771e101c196f31e5be93dffaa247d6994ebe490e303c1e55f1970ed"
     },
 
     beforeSend: function(jqXHR) {
